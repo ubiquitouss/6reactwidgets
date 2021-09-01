311b52365722134c3b4cb2b3a9d42617f9d9add9
ubiquitouss <ubiquitouss@yahoo.com>
2021-09-01 10:05:41 +0600

    final widgtet traslation is added

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -1,16 +1,53 @@
+import axios from 'axios';
 import React, {useState, useEffect} from 'react';
 
 
 const Convert = ({language,text}) => {
 
+    const [translated, setTranslated] = useState('');
+    const [debouncedText, setDebouncedText] = useState(text);
+
+
     useEffect(() => {
-        console.log('New language or text')
-    }, [language, text])
+        const timerId = setTimeout(()=>{
+            setDebouncedText(text)
+        },500);
+
+        return () => {
+            clearTimeout(timerId)
+        }
+    },[text]);
+
+
+    useEffect(() => {
+
+        const doTranslation = async () => {
+        //    {data}, this property is from google. we can store the whole result in a different variable
+        //  but here we are just taking the value that is {data}
+         const {data} = await axios.post('https://translation.googleapis.com/language/translate/v2',{},{
+            params: {
+                q: debouncedText,
+                target: language.value,
+                key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM'
+
+            },
+        }
+    );
+    setTranslated(data.data.translations[0].translatedText)
+
+};
+// at last we are calling the whole function
+  doTranslation()        
+}, [language, debouncedText])
    
     // if we put any language or text we will run useEffect function
 
 
-    return <div />
+    return (
+        <div>
+            <h1 className = "ui header">{translated}</h1>
+        </div>
+    )
 }
 
 
